c5d6b6bc2a8dd173cc12e96d5b3ca1fad3df066e
devSobuj910 <ftsobuj222@gmail.com>
2022-11-27 22:29:07 +0600

    basic server running and mongo test

--- index.js
+++ index.js
@@ -1,24 +1,75 @@
 const express = require('express');
-const cors= require("cors")
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 const app = express();
-const port=process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+var jwt = require('jsonwebtoken')
 
 
 
-// midleware
+
+// middleware
 app.use(cors());
 app.use(express.json());
 
 
-// genarel server
 
-app.get('/', (req, res) => {
-  res.send('this is hea_Easy server');
-});
 
-app.listen(port, () => {
- 
-    console.log(`Server listening on port ${port}`)
-    
 
+
+
+
+
+
+//mongodb code is  here
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jnabqpv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const run=async()=>{
+  try{
+    //usercollection
+    const usercollection=client.db("heaeasy").collection("users")
+    const users={
+      test:"test"
+    }
+const resut=await usercollection.insertOne(users)
+
+
+
+
+  }
+  finally{
+
+  }
+}
+
+run().catch(err=>console.log(err))
+
+
+
+
+
+
+
+
+
+
+
+
+//genarel server
+
+
+app.get('/', async (req, res) => {
+  res.send(" hea-easy  server is running");
 })
+
+app.listen(port, () => console.log(` hea-esay server running on ${port}`))
+
+
+
+
