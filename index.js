e1a64cc5880d9bb4031f2e1b508c4cba14ee2253
devSobuj910 <ftsobuj222@gmail.com>
2022-11-30 08:53:01 +0600

    product add

--- index.js
+++ index.js
@@ -19,40 +19,6 @@ app.use(express.json());
 
 
 
-// const categories = require('./data/categories.json');
-// const news = require('./data/news.json');
-
-
-// app.get('/news-categories', (req, res) => {
-//     res.send(categories)
-// });
-
-// app.get('/category/:id', (req, res) => {
-//     const id = req.params.id;
-//     if (id === '08') {
-//         res.send(news);
-//     }
-//     else {
-//         const category_news = news.filter(n => n.category_id === id);
-//         res.send(category_news);
-//     }
-// })
-
-// app.get('/news', (req, res) =>{
-//     res.send(news);
-// });
-
-// app.get('/news/:id', (req, res) => {
-//     const id = req.params.id;
-//     const selectedNews = news.find(n => n._id === id);
-//     res.send(selectedNews);
-// });
-
-
-// //mongodb code is  here
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jnabqpv.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -60,232 +26,50 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const run=async()=>{
   try{
   //   //usercollection
-    const usercollection=client.db("heaeasy").collection("users")
+    const usersCollection=client.db("heaeasy").collection("users")
+   const homesCollection = client.db('heaeasy').collection('homes')
+   const orderCollection=client.db("heaeasy").collection("orders")
+   
 
-    const productCatagoris=client.db("heaeasy").collection("prodauctCatagory")
+
+ 
+
+   app.post("/orders",async(req,res)=>{
+    const order=req.body
+   
+    const result=await orderCollection.insertOne(order)
+    console.log(result)
+    res.send(result)
+
+   })
 
 
 
 
-    //get all catagory
 
-    app.get("/catagory",async(req,res)=>{
-      const query={}
-      const result=await productCatagoris.find(query).toArray()
-      res.send(result)
 
+    
+
+    // Save user email & generate JWT
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const filter = { email: email }
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: user,
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc, options)
+      console.log(result)
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d',
+      })
+      console.log(token)
+      res.send({ result, token })
     })
 
-
-
-
-
-
-
-
-
-
-
-
-  //   const srviceCollection = client.db("ginusServerCollection").collection("services")
- 
-
-
-
-  //   const orderCollection = client.db("ginusServerCollection").collection("orders");
-     
-  //   // all data get
-    
-    
-  //   app.get("/services",async(req,res)=>{
-  //       const query={};
-  //       const services=await srviceCollection.find(query).toArray();
-  //       res.send(services);
-    
-       
-  //   })
-    
-  //   // get spacificdata    
-    
-  //   app.get("/services/:id", async(req,res)=>{
-  //       const id=req.params.id;
-  //       const query={_id:ObjectId(id)};
-  //       const services=await srviceCollection.findOne(query);
-  //       res.send(services);
-    
-    
-    
-  //   })
-    
-    
-
-  //   app.get("/cata/:meseg", async(req,res)=>{
-  //  const meseg=req.params.meseg;
-  //  const query={meseg:meseg};
-  //  const ress=await orderCollection.find(query).toArray()
-  //  res.send(ress)
-
-
-  //   })
-
-    
-    
-    //order post
-    
-    
-    // app.post("/orders",  async(req,res)=>{
-    //     //  console.log(req.headers.authorization)
-    // const order = req.body;
-    // const result=await  orderCollection.insertOne(order);
-    //     res.send(result);
-    
-    // })
-    
-    
-    
-    ///get first all oreder then spacific order collection
-    
-    
-    // app.get("/orders", async(req,res)=>{
-    
-    // const decoded=req.decoded;
-    // if(decoded.email !== req.query.email){
-    //     res.status(401).send({messege:"unauthorized"});
-    // }
-    
-    //  let query={};
-    //     if(req.query.email){
-    //         query.email=req.query.email;
-    //     }
-        
-    //     const orders=await orderCollection.find(query).toArray();
-    //     res.send(orders);
-    
-    
-    // })
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //uptgrade data
-    
-    
- 
-    
-    
-    
-    
-    
-    
-    ///get jwt token from clint  side
-    
-    
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    // app.get('/news-categories',async (req, res) => {
-      
-    //  const query={};
-    //  const result=await catagorycollection.find(query).toArray()
-     
-
-    //     res.send(result)
-    // });
-    
-    // app.get('/catagory/:id', async(req, res) => {
-    //     const id = req.query.id;
-    //   const query={_id:ObjectId(id)}
-    //         const categorynews =await Productcollection.find(query).toArray()
-    //         res.send(categorynews);
-        
-    // })
-    
-    // app.get('/news', async(req, res) =>{
-
-    //   const query={};
-    //   const result=await Productcollection.find(query).toArray()
-    //     res.send(result);
-    // });
-    
-    // app.get('/news/:id', async(req, res) => {
-    //     const id = req.params.id;
-    //     const query={_id: ObjectId(id)}
-    //     const selectedNews =await Productcollection.find(query).toArray();
-    //     res.send(selectedNews);
-    // });
-    
-    
-    // //mongodb code is  here
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-app.put("/user/:email",async(req,res)=>{
-const email=req.params.email
-const  user=req.body
-const filter={email:email}
-const option={upsert:true}
-const updoc={
-  $set:user,
-}
-const result=await usercollection.updateOne(filter,updoc ,option)
-
-
-const token=jwt.sign(user,process.env.ACCES_TOKEN,{expiresIn:"1h"})
-res.send({result,token})
-
-})
-
-
-
-
-
-
-
+    console.log('Database Connected...')
 
 
   }
@@ -315,7 +99,5 @@ app.get('/', async (req, res) => {
 })
 
 app.listen(port, () => console.log(` hea-esay server running on ${port}`))
-
-
 
 
