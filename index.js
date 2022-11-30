7c74d55ed19c0c7ae57d037e6bf7cdd65ef17afa
devSobuj910 <ftsobuj222@gmail.com>
2022-11-30 20:29:41 +0600

    load all  user and buyers

--- index.js
+++ index.js
@@ -18,58 +18,295 @@ app.use(express.json());
 
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jnabqpv.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 const run=async()=>{
-  try{
-  //   //usercollection
-    const usersCollection=client.db("heaeasy").collection("users")
-   const homesCollection = client.db('heaeasy').collection('homes')
-   const orderCollection=client.db("heaeasy").collection("orders")
-   
+  try {
+    const userLoginCollection = client.db("heaeasy").collection("registerData")
+    const categoryLoginCollection = client.db("heaeasy").collection("category")
+    const productsDataCollection = client.db("heaeasy").collection("productCollection");
+    const bookingDataCollection = client.db('heaeasy').collection('bookingData')
+    const reportDataCollection = client.db('heaeasy').collection('reportData')
+    const boostProductsCollection = client.db('heaeasy').collection('boostProduct')
 
+    // register time post 
+    app.post('/registerData', async (req, res) => {
+      const userData = req.body;
+      const result = await userLoginCollection.insertOne(userData)
+      res.send(result)
+    });
 
- 
-
-   app.post("/orders",async(req,res)=>{
-    const order=req.body
-   
-    const result=await orderCollection.insertOne(order)
-    console.log(result)
-    res.send(result)
-
-   })
-
-
-
-
-
-
-    
-
-    // Save user email & generate JWT
-    app.put('/user/:email', async (req, res) => {
-      const email = req.params.email
-      const user = req.body
-      const filter = { email: email }
-      const options = { upsert: true }
-      const updateDoc = {
-        $set: user,
-      }
-      const result = await usersCollection.updateOne(filter, updateDoc, options)
-      console.log(result)
-
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '1d',
-      })
-      console.log(token)
-      res.send({ result, token })
+    //get all user
+    app.get('/allUser', async (req, res) => {
+      const query = {}
+      const allUser = await userLoginCollection.find(query).toArray()
+      res.send(allUser)
     })
 
-    console.log('Database Connected...')
+    app.get('/userData/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userLoginCollection.find(query).toArray();
+      res.send(result)
+
+    })
+
+    //social
+    app.get("/socialLogin/:email", async (req, res) => {
+      const email = req.params.email;
+      //   console.log(email);
+      const query = { email: email };
+      const curser = userLoginCollection.find(query);
+      const result = await curser.toArray();
+      const arraysLength = result;
+      if (arraysLength.length > 0) {
+        return res.send("old user");
+      }
+      const newUser = {
+        email: email,
+        name: email,
+        accountType: "Buyer",
+      };
+      //   console.log(email)
+      const createUser = await userLoginCollection.insertOne(newUser);
+      res.send(createUser);
+      //   console.log(createUser);
+    });
+
+
+    //category get
+
+    app.get('/category', async (req, res) => {
+      const query = {}
+      const result = await categoryLoginCollection.find(query).toArray()
+      res.send(result)
+    });
+
+
+    app.get("/CarCategory", async (req, res) => {
+      const queryData = req.query.model;
+      if (queryData == null) {
+        const query = { model: 'Bm-W' };
+        const data = await categoryLoginCollection.find(query).toArray();
+        return res.send(data);
+      }
+
+      const query = { model: queryData };
+      const curser = categoryLoginCollection.find(query);
+      const result = await curser.toArray();
+      res.send(result);
+    });
+
+    // car post
+    app.post("/products", async (req, res) => {
+      const productsData = req.body;
+      const result = await productsDataCollection.insertOne(productsData);
+      res.send(result);
+    });
+
+    //get my product list
+
+    app.get('/products/:email', async (req, res) => {
+      const productEmail = req.params.email;
+      //  console.log(productEmail)
+      const query = { seller: productEmail }
+      const result = productsDataCollection.find(query);
+      const curser = await result.toArray()
+      res.send(curser)
+    })
+
+    //boostProducts
+    app.post('/boostProduct', async (req, res) => {
+      const boostProduct = req.body
+      const result = await boostProductsCollection.insertOne(boostProduct)
+      res.send(result)
+    })
+
+    // product delete
+    app.delete('/productsData/:id', async (req, res) => {
+      const ID = req.params.id;
+      const query = { _id: ObjectId(ID) }
+      const result = await productsDataCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    // car category wise get
+    app.get("/allCars", async (req, res) => {
+
+      const queryData = req.query.model;
+      if (queryData == null) {
+        const query = { carType: 'Porsche' };
+        const data = await productsDataCollection.find(query).toArray();
+        return res.send(data);
+      }
+
+      const query = { carType: queryData };
+      const curser = productsDataCollection.find(query);
+      const result = await curser.toArray();
+      res.send(result);
+    });
+
+
+
+
+
+
+
+
+
+    //booking data post
+    app.post('/bookingData', async (req, res) => {
+      const bookingData = req.body;
+      const result = await bookingDataCollection.insertOne(bookingData)
+      res.send(result)
+    })
+
+    // get myOrder 
+    app.get('/bookingData/:email', async (req, res) => {
+
+      const bookingEmail = req.params.email
+      const query = { email: bookingEmail }
+      const result = bookingDataCollection.find(query)
+      const curser = await result.toArray()
+      res.send(curser)
+
+    })
+
+    //report post
+    app.post('/reportData', async (req, res) => {
+      const reportData = req.body
+      const result = await reportDataCollection.insertOne(reportData)
+      res.send(result)
+    })
+
+    //all report list get
+    app.get('/allreportData', async (req, res) => {
+      const query = {}
+      const result = await reportDataCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    //delete Report item
+    app.delete('/allReportDelete/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await reportDataCollection.deleteOne(query)
+      res.send(result)
+
+    })
+
+    // admin
+    // app.get('/usersAdmin/:email', async (req, res) => {
+    //   const email = req.params.email;
+    //    const query = { email };
+    //    const user = await userLoginCollection.findOne(query);
+    //    res.send({ isAdmin: user?.accountType === 'admin' });
+    //  });
+
+    app.get("/adminState/:email", async (req, res) => {
+      console.log(req.params.email);
+      const query = { email: req.params.email };
+      const curser = await userLoginCollection.findOne(query);
+      if (curser?.accountType === "Admin") {
+        console.log("admin");
+        return res.send(true);
+      }
+      console.log("not admin");
+      res.send(false);
+    });
+
+
+    //seller
+    // app.get('/usersSeller/:email', async (req, res) => {
+    //   const email = req.params.email;
+    //   const query = { email };
+    //   const user = await userLoginCollection.findOne(query);
+    //   res.send({ isSeller: user?.accountType === 'Seller' });
+    // });
+
+    app.get("/sellerState/:email", async (req, res) => {
+      console.log(req.params.email);
+      const query = { email: req.params.email };
+      const curser = await userLoginCollection.findOne(query);
+      if (curser?.accountType === "Seller") {
+        console.log("Seller");
+        return res.send(true);
+      }
+      console.log("not Seller");
+      res.send(false);
+    });
+
+    //buyer
+    // app.get('/usersBuyer/:email', async (req, res) => {
+    //   const email = req.query.email;
+    //    const query = { email };
+    //    const user = await userLoginCollection.findOne(query);
+    //    res.send({ isBuyer: user?.accountType === 'Buyer' });
+    //  });
+
+    app.get("/buyerState/:email", async (req, res) => {
+      console.log(req.params.email);
+      const query = { email: req.params.email };
+      const curser = await userLoginCollection.findOne(query);
+      if (curser?.accountType === "Buyer") {
+        console.log("buyer");
+        return res.send(true);
+      }
+      console.log("not buyer");
+      res.send(false);
+    });
+
+
+    //all seller
+    app.get("/allSellers", async (req, res) => {
+      // const queryData = req.params.accountType;
+      const query = { accountType: 'Seller' };
+      const curser = userLoginCollection.find(query);
+      const result = await curser.toArray();
+      res.send(result);
+    });
+
+    //delete seller
+    app.delete('/allSellerDelete/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await userLoginCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    //get all Buyer
+    app.get("/allBuyers", async (req, res) => {
+      // const queryData = req.params.accountType;
+      const query = { accountType: 'Buyer' };
+      const curser = userLoginCollection.find(query);
+      const result = await curser.toArray();
+      res.send(result);
+    });
+
+    // delete Buyer
+    app.delete('/allBuyerDelete/:id', async (req, res) => {
+
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await userLoginCollection.deleteOne(query)
+      res.send(result)
+
+    })
+
+    app.get("/user/:email", async (req, res) => {
+      console.log(req.params.email);
+      const query = { email: req.params.email };
+      const curser = await userLoginCollection.findOne(query);
+      if (curser) {
+        return res.send(curser);
+      }
+
+      res.send(false);
+    });
+
+
 
 
   }
